fd0065e6d20ef37a51bfd7b5b75518c9fbf4d28a
ihateblueb <ihateblueb@proton.me>
2026-05-15 11:39:18 -0400

    Add forge mod start

--- server/definition/application.go
+++ server/definition/application.go
@@ -5,11 +5,10 @@ import "gorm.io/gorm"
 type Application struct {
 	gorm.Model
 
-	Id                       string  `json:"id"`
-	Username                 string  `json:"username"` // Ingame username
-	Age                      int     `json:"age"`
-	WhereDidYouFindTheServer string  `json:"where_did_you_find_the_server"`
-	Bio                      string  `json:"bio"`
-	Invite                   *string `json:"invite"` // Optional, allows instant approval if valid
-	Status                   int     `json:"status"` // 0: Pending, 1: Rejected, 2: Approved
+	Id                       string `json:"id"`
+	Username                 string `json:"username"` // Ingame username
+	Age                      int    `json:"age"`
+	WhereDidYouFindTheServer string `json:"where_did_you_find_the_server"`
+	Bio                      string `json:"bio"`
+	Status                   int    `json:"status"` // 0: Pending, 1: Rejected, 2: Approved
 }
